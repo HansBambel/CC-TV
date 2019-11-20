ed979c43023cff73b299b2d7c7d9ea46ffe59f18
Kevin Trebing <Kevin.Trebing@gmx.net>
2019-11-20 18:24:48 +0100

    Started with website. Images are not updated properly yet

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, url_for, redirect
+import os
+import overlayLib
+
+
+imageFolder = os.path.join("static", "images")
+cities = [
+    {
+        "query": "Buenos Aires, Argentina",
+        "size": 24.34,
+        "image": os.path.join(imageFolder, "defaultBigCity.png")
+    },
+    {
+        "query": "Nairobi, Kenya",
+        "size": 20.21,
+        "image": os.path.join(imageFolder, "defaultSmallCity.png")
+    }
+]
+overlayIm = os.path.join(imageFolder, "defaultOverlay.png")
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.route("/", methods=['POST'])
+def compare():
+    city1Query = request.form["City 1"]
+    city2Query = request.form["City 2"]
+    print(city1Query, " ", city2Query)
+    if city1Query is "" or city2Query is "":
+        # TODO display warning
+        pass
+    else:
+        # Use the input, create new overlay, refresh page
+        city1, size1 = overlayLib.getCityOutline(city1Query)
+        city2, size2 = overlayLib.getCityOutline(city2Query)
+        overlay = overlayLib.overlayCities(city1, size1, city2, size2)
+        size1 = round(size1, 2)
+        size2 = round(size2, 2)
+        global cities
+        global overlayIm
+        cities = [
+            {
+                "query": city1Query if size1 > size2 else city2Query,
+                "size": size1 if size1 > size2 else size2,
+                "image": os.path.join(imageFolder, "BigCity.png")
+            },
+            {
+                "query": city2Query if size1 > size2 else city1Query,
+                "size": size2 if size1 > size2 else size1,
+                "image": os.path.join(imageFolder, "SmallCity.png")
+            }
+        ]
+        os.path.join(imageFolder, "Overlay.png")
+    return redirect(url_for("home"))
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", defaultCities=cities, overlayIm=overlayIm)
+
+
+@app.after_request
+def add_header(response):
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Expires'] = '0'
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ overlayLib.py
@@ -0,0 +1,100 @@
+import geopandas
+import urllib
+import shapely
+import matplotlib.pyplot as plt
+from math import sin, cos, sqrt, atan2, radians
+import json
+from PIL import Image
+
+
+def getDistance(lat1, lon1, lat2, lon2):
+    R = 6373.0
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
+
+def getCityOutline(query: str):
+    openStreetMap_base_url = "https://nominatim.openstreetmap.org/search"
+    url = openStreetMap_base_url + '?' + urllib.parse.urlencode({
+        "q": query,
+        "format": "json",
+        "polygon_geojson": 1
+    })
+
+    response = urllib.request.urlopen(url).read()
+    responseJson = json.loads(response)
+
+    # find first multipolygon/polygon result (most likely the correct one)
+    for shape in responseJson:
+        if isinstance(shapely.geometry.shape(shape["geojson"]), shapely.geometry.MultiPolygon):
+            cityShape = shapely.geometry.shape(shape["geojson"])
+            lat1, long1, lat2, long2 = cityShape.bounds
+            distance = getDistance(lat1, long1, lat2, long2)
+            cityGDF = geopandas.GeoDataFrame({
+                "geometry": cityShape
+            })
+            return cityGDF, distance
+        elif isinstance(shapely.geometry.shape(shape["geojson"]), shapely.geometry.Polygon):
+            cityShape = shapely.geometry.shape(shape["geojson"])
+            lat1, long1, lat2, long2 = cityShape.bounds
+            distance = getDistance(lat1, long1, lat2, long2)
+            cityGDF = geopandas.GeoDataFrame({
+                "geometry": cityShape
+            }, index=[0])
+            return cityGDF, distance
+    # If nothing found return None
+    return None, None
+
+
+def overlayCities(city1, size1, city2, size2):
+    if size1 > size2:
+        bigCity = city1
+        bigSize = size1
+        smallCity = city2
+        smallSize = size2
+    else:
+        bigCity = city2
+        bigSize = size2
+        smallCity = city1
+        smallSize = size1
+    # Save the cities as images
+    bigCity.plot(color="red")
+    plt.axis("off")
+    plt.savefig("static/images/BigCity.png", transparent=True)
+    plt.close()
+    smallCity.plot(alpha=0.7)
+    plt.axis("off")
+    plt.savefig("static/images/SmallCity.png", transparent=True)
+    plt.close()
+    # Load images with Pillow
+    bigCityIm = Image.open("static/images/BigCity.png")
+    smallCityIm = Image.open("static/images/SmallCity.png")
+    oldWidth, oldHeight = smallCityIm.size
+    newWidth = int(oldWidth * smallSize / bigSize)
+    newHeight = int(oldHeight * smallSize / bigSize)
+    # Scale down the smaller city
+    smallCityImScaled = smallCityIm.resize((newWidth, newHeight))
+    # Find center of bigger city
+    bigCityWidth, bigCityHeight = bigCityIm.size
+    bigCenterW = int(bigCityWidth / 2)
+    bigCenterH = int(bigCityHeight / 2)
+    topLeftX = bigCenterW - int(newWidth / 2)
+    topLeftY = bigCenterH - int(newHeight / 2)
+    botRightX = bigCenterW + (newWidth - int(newWidth / 2))
+    botRightY = bigCenterH + (newHeight - int(newHeight / 2))
+    # Paste the small city onto the bigger one (the mask key-word reserves the transparency of the png)
+    bigCityIm.paste(smallCityImScaled, (topLeftX, topLeftY, botRightX, botRightY), mask=smallCityImScaled)
+    bigCityIm.save("static/images/Overlay.png", "png")
+    #     bigCityIm.show()
+    return bigCityIm
